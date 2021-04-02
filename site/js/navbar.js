f57c357aab8411a7b5dcaef2bbef840e7969257d
rubenruvalcabac <rubenruvalcabac@hotmail.com>
2021-04-02 13:04:48 -0600

    navbar sticky

--- site/js/navbar.js
+++ site/js/navbar.js
@@ -5,3 +5,25 @@ function openNav() {
 function closeNav() {
   document.getElementById("navbar__overlay").style.width = "0";
 }
+
+function initNavbarScrollHide() {
+  // Looks for the hider element, when this hider element is scrolled out, the navbar will be unsticky
+  const hiderElement = document.getElementById("navbar__scroll-hide");
+  if (!hiderElement) return;
+
+  const navBarElement = document.getElementById("navbar");
+  if (!navBarElement) return;
+
+  navBarElement.style.position = "sticky";
+  const hiderElementTop = hiderElement.getBoundingClientRect().top;
+
+  window.addEventListener("scroll", (e) => {
+    if (window.pageYOffset > hiderElementTop) {
+      navBarElement.style.position = null;
+    } else {
+      navBarElement.style.position = "sticky";
+    }
+  });
+}
+
+initNavbarScrollHide();
